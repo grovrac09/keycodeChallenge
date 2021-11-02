9f635b14daea2b64613cb9308144c9a3af21c67d
Rachel Groves <grovrac09@gmail.com>
2021-11-02 12:46:14 +0000

    basic styling

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const key = document.getElementById("key");
 const placement = document.getElementById("placement");
 const which = document.getElementById("which");
 const code = document.getElementById("code");
+const header = document.getElementById("header");
 
 document.addEventListener("keydown", (event) => {
     key.textContent = `${event.key}`;
     placement.textContent = `${event.location}`;
     which.textContent = `${event.which}`;
     code.textContent = `${event.code}`;
+    header.textContent = `${event.which}`;
 })
 
 
